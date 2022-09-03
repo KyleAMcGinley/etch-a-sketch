8004de04fd1b078d0c97045b3e1cbb56809886a3
Kyle McGinley <kyleamcginley@gmail.com>
2022-09-03 10:49:08 -0400

    Succesfully named all of the grid divs
    Going to add style

--- myscript.js
+++ myscript.js
@@ -1,7 +1,7 @@
 //Making 16x16 grid using DOM
 //Going to use a loop function to add 16 <div>s to the #container
 const container = document.querySelector('div#container');
-var divs = document.querySelectorAll('div');
+const newDiv = document.createElement('div');
 
 //This function uses a loop to create <divs> and add it to container
 function makeGrid(){
@@ -11,5 +11,17 @@ function makeGrid(){
         
     }
 }
+makeGrid();
 
-makeGrid()s
+//Using function to add a class list to the 16 new divs
+
+//Assigned divs variable to all the divs within the container -- results in a nodelist
+const divs = document.querySelectorAll('#container > div');
+//Using a loop function to name each div within the node list
+function namingGrids(){
+    for (i = 0; i <= divs.length - 1; i++){
+        divs[i].classList.add('grid-boxes');
+    }
+}
+namingGrids();
+
